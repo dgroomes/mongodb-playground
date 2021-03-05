676947365852d9c839996e035656e7c06bd20b06
David Groomes <dwgroomes@gmail.com>
2021-03-05 16:04:31 -0600

    Incrementally incorporate new documents

--- queries/zips-average-incremental.js
+++ queries/zips-average-incremental.js
@@ -25,9 +25,8 @@ db.zips.updateMany(
 const lastLoadedTime = db.app_meta_data.findOne().last_loaded_time
 print(`Last loaded time: ${lastLoadedTime}`)
 
-// Work in process (actually merge the data into the collection)
 // Incorporate the new ZIP records into the collection of ZIP areas grouped by city.
-const cursor = db.zips.aggregate(
+db.zips.aggregate(
   [
     {
       $match: {
@@ -38,15 +37,107 @@ const cursor = db.zips.aggregate(
     },
     {
       $group: {
-        _id: null,
-        size: {
-          $sum: 1
+        "_id": {city: "$city", state: "$state"},
+        zip_areas: {
+          $addToSet: "$$CURRENT"
         }
+      }
+    },
+    {
+      "$set": {
+        lastModified: "$$NOW"
+      }
+    },
+    {
+      $merge: {
+        into: "zips_grouped_by_city",
+        whenMatched: [
+          {
+            $set: {
+              zip_areas: {
+                $setUnion: ["$zip_areas", "$$new.zip_areas"]
+              },
+              lastModified: "$$new.lastModified"
+            }
+          }
+        ]
       }
     }
   ]
 )
-printjson(`${cursor.next().size} records were incrementally incorporated.`)
 
 // Commit completed work using "last loaded time" field.
 db.app_meta_data.updateOne({_id: 1}, [{$set: {last_loaded_time: "$$NOW"}}], {upsert: true})
+
+// COPIED FROM zip-average.js
+// Next, compute the average.
+db.zips_grouped_by_city.aggregate([
+  {
+    $project: {
+      city_zip_areas: {$size: "$zip_areas"},
+      city_pop: {$sum: "$zip_areas.pop"},
+    }
+  },
+  {
+    $addFields: {
+      avg_zip_area_pop_across_city: {
+        $trunc: {
+          $divide: ["$city_pop", "$city_zip_areas"]
+        }
+      }
+    }
+  },
+  {$out: "zips_avg_pop_by_city"}
+])
+
+let cursorAvgByCity = db.zips_avg_pop_by_city.find().sort({city_pop: -1})
+
+function printAFewRecords(cursor) {
+  for (let i = 0; cursor.hasNext() && i < 3; i++) {
+    printjson(cursor.next())
+  }
+  print()
+}
+
+print("Average population of the ZIP areas for each city")
+printAFewRecords(cursorAvgByCity)
+
+// Next, group the city-aggregated ZIP area summaries by state into a new collection. Why? Well, I think it will be useful
+// de-duplication.
+db.zips_avg_pop_by_city.aggregate([
+  {
+    "$group": {
+      _id: "$_id.state",
+      city_aggregated_zip_area_summaries: {
+        $addToSet: "$$CURRENT"
+      }
+    }
+  },
+  {$out: "zips_grouped_by_state"}
+])
+
+// Compute state-level ZIP area averages
+db.zips_grouped_by_state.aggregate([
+  {
+    "$project": {
+      _id: "$_id",
+      state_zip_areas: {$sum: "$city_aggregated_zip_area_summaries.city_zip_areas"},
+      state_pop: {$sum: "$city_aggregated_zip_area_summaries.city_pop"}
+    }
+  },
+  {
+    $addFields: {
+      avg_zip_area_pop_across_state: {
+        $trunc: {
+          $divide: ["$state_pop", "$state_zip_areas"]
+        }
+      }
+    }
+  },
+  {$out: "zips_avg_pop_by_state"}
+])
+
+let cursorAvgByState = db.zips_avg_pop_by_state.find().sort({state_pop: -1})
+
+print("Average population of the ZIP areas for each state")
+printAFewRecords(cursorAvgByState)
